aa5c6379bb938ddfe9ab021d8416bc73eb6c9cef
Caven15 <cavenditch15@gmail.com>
2024-01-11 13:51:59 +0100

    update bll

---
+++ DemoToken.BLL/Services/UtilisateurService.cs
@@ -0,0 +1,26 @@
+﻿using demoToken.DAL.Interfaces;
+using DemoToken.BLL.Interfaces;
+using DemoToken.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoToken.BLL.Services
+{
+    public class UtilisateurService : IUtilisateurService
+    {
+        private readonly IUtilisateurRepository
+
+        public UtilisateurModel LoginUtilisateur(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterUtilisateur(UtilisateurModel utilisateur)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
